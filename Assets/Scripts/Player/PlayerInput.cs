a2bfe67fcf15e735968d2b111e7d2d5ef72936d1
erfan <erfantavakoly@comp.iust.ac.ir>
2018-10-27 11:49:18 +0330

    Add basic movement for the player

---
+++ Assets/Scripts/Player/PlayerInput.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// This class handle inputs based on the different platforms for the player
+/// </summary>
+public class PlayerInput : MonoBehaviour
+{
+    private float _horizontal;
+    private float _Vertical;
+   
+    public string myString { get; set; }
+
+    void Awake()
+    {
+
+    }
+
+    void Update()
+    {
+        _horizontal = Input.GetAxis("Horizontal");
+        _Vertical = Input.GetAxis("Vertical");
+    }
+    public float GetHorizontal()
+    {
+        return _horizontal;
+    }
+    public float GetVertical()
+    {
+        return _Vertical;
+    }
+
+}
---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// This class handle movement of the player based on the rigidbody that is assigned.
+/// </summary>
+
+[RequireComponent(typeof(PlayerInput))]
+public class PlayerMovement : MonoBehaviour
+{
+
+    public Rigidbody2D RigidBody;
+
+    private PlayerInput _playerInput;
+
+    void Awake()
+    {
+        _playerInput = GetComponent<PlayerInput>();
+    }
+
+    void Start()
+    {
+
+
+    }
+
+    void Update()
+    {
+
+    }
+    void FixedUpdate()
+    {
+        RigidBody.velocity = new Vector2(_playerInput.GetHorizontal(), _playerInput.GetVertical());
+    }
+}
