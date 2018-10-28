f7e02ff14aeb8a294722d86dee29a60dee999c73
erfan <erfantavakoly@comp.iust.ac.ir>
2018-10-28 10:02:21 +0330

    Add basic art for items Add Player, Pcikuable Item and obstacle Prefab improve script foldering add some class for pickuping machanic

---
+++ Assets/Scripts/Player/PickUp/PickupStrategy.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupStrategy : MonoBehaviour {
+
+    public System.Action<PickupableObject> ItemPickedUpCallBack;
+	void Start () {
+		
+	}
+	
+	
+	void Update () {
+		
+	}
+
+    protected void ItemPickedup(PickupableObject pickupable)
+    {
+        ItemPickedUpCallBack?.Invoke(pickupable);
+        Debug.Log("Item PickedUp: "+pickupable.name);
+    }
+}
---
+++ Assets/Scripts/Player/PickUp/PickupableObject.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupableObject : MonoBehaviour {
+
+	public Vector2 GetCenterPosition()
+    {
+        return transform.position;
+    }
+}
